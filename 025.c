f48dfa68eb32cb9da251cd90ea2a80bde7f32434
wong4835 <57549949+wong4835@users.noreply.github.com>
2020-01-21 10:39:43 +0900

    2020 01 21

---
+++ 025.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+    int x = 1;
+    int y = 2;
+    int z = 3;
+
+    if(x == y)printf("x is same the y.\n");
+    if(x != y)printf("x is not same the y.\n");
+    if(x > y)printf("x is bigger the y.\n");
+    if(x < y)printf("x is smaller the y.\n");
+    if(x >= y)printf("y is bigger or same the x.\n");
+    if(x <= y)printf("y is smaller or same the x.\n");
+}
